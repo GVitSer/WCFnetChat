928ebc0185fbe4d98a0faf00cbb5de9294c6267e
GVitSer <gvs.bzn@mail.ru>
2021-09-07 23:09:46 +0300

    Initial commit

---
+++ WCFnetChat/IServiceChat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFnetChat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServiceChatCallBack))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect(string Name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg, int id);
+    }
+
+    public interface IServiceChatCallBack
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallBack(string msg);
+    }
+}
